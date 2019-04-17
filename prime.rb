097180584bcaa7765b14ec4f15c1be355ca5596e
Meg Galvez <meg@oakmaine.com>
2019-04-17 16:43:23 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,5 @@
 def prime?(num)
-  # check if num is less than 2.
   false if num < 2
-  
-  # check if num is divisible by any number
-  # between 2 to (num - 1)
-  # return false on the first instance
   (2...num).each do |factor|
     if (num % factor).zero? 
       return false
